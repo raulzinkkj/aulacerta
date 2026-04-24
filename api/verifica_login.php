ee1aeba84c9783ae37988f514e4153174478b782
raulzinkkj <karvatraul@gmail.com>
2026-04-24 17:06:21 -0300

    123

--- api/verifica_login.php
+++ api/verifica_login.php
@@ -6,7 +6,6 @@ include '../conexao/conexao.php';
 $email_usuario = $_POST['email_usuario'];
 $senha_usuario = $_POST['senha_usuario'];
 
-$senhaHash = password_hash($senha_usuario, PASSWORD_DEFAULT);
 
 $sql = "SELECT * FROM usuario WHERE email_usuario = :email_usuario";
 $stmt = $conexao->prepare($sql);
@@ -15,7 +14,7 @@ $stmt->execute();
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($user && password_verify($senhaHash, $user['senha_usuario'])) {
+if ($user && password_verify($senha_usuario, $user['senha_usuario'])) {
     $_SESSION['email_usuario'] = $user['email_usuario'];
     $_SESSION['id_usuario'] = $user['id_usuario'];
     $_SESSION['nome_usuario'] = $user['nome_usuario'];
